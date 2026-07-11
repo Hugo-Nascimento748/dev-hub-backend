46f1d2075d6e2aad259b565514ca368393bdefab
Hugo-Nascimento748 <nascimentohugo748@gmail.com>
2026-07-11 12:42:43 -0300

    correção de swagger web

--- src/main/java/com/devhub/api/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/devhub/api/infrastructure/config/OpenApiConfig.java
@@ -13,10 +13,14 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addServersItem(new io.swagger.v3.oas.models.servers.Server().url("http://localhost:8081"))
+                .addServersItem(new io.swagger.v3.oas.models.servers.Server()
+                        .url("https://dev-hub-backend-frmz.onrender.com")
+                        .description("Servidor de Produção"))
+                .addServersItem(new io.swagger.v3.oas.models.servers.Server()
+                        .url("http://localhost:8081")
+                        .description("Servidor Local"))
                 .info(new io.swagger.v3.oas.models.info.Info()
                         .title("DevResources Hub API")
-                        .version("1.0")
-                        .description("Plataforma de curadoria de links"));
+                        .version("1.0"));
     }
 }
